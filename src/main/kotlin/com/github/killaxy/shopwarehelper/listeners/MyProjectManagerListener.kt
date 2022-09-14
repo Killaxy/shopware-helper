ab63138442fea1bdcaada70462816fb17837be56
GitHub Action <action@github.com>
2022-09-14 09:10:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/killaxy/shopwarehelper/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/killaxy/shopwarehelper/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.killaxy.shopwarehelper.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.killaxy.shopwarehelper.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/killaxy/shopwarehelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/killaxy/shopwarehelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.killaxy.shopwarehelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.killaxy.shopwarehelper.MyBundle
 
 class MyApplicationService {
 
